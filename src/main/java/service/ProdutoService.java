342dda973f939519968118f148532c39ba398257
MiguelSteinmetz <miguelcostasteinmetz@gmailcom>
2026-03-31 21:20:35 -0300

    primeiro codigo

---
+++ src/main/java/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package service;
+
+import model.Produto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoService {
+    private List<Produto> listaProdutos = new ArrayList<>();
+
+    public void adicionar(Produto p) {
+        listaProdutos.add(p);
+    }
+
+    public List<Produto> listarTodos() {
+        return listaProdutos;
+    }
+
+    public Produto buscarPorId(int id) {
+        for (Produto p : listaProdutos) {
+            if (p.getId() == id) return p;
+        }
+        return null;
+    }
+
+    public boolean deletarPorId(int id) {
+        Produto p = buscarPorId(id);
+        if (p != null) {
+            return listaProdutos.remove(p);
+        }
+        return false;
+    }
+
+    public boolean idJaExiste(int id) {
+        return buscarPorId(id) != null;
+    }
+}
