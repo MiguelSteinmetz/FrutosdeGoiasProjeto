ea18c56678d9e8f14915425533d13e793d3ab2e4
Erik Tontini <erik_pelissari123@hotmail.com>
2026-04-06 19:50:08 -0300

    teste conecçao banco de dados

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,11 @@
 
 package main;
 
+import config.FlyWayconfig;
+
 public class Main {
     public static void main(String[] args) {
+        FlyWayconfig.migrate();
         Sistema sistema = new Sistema();
         sistema.iniciar();
     }
