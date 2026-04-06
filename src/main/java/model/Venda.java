a06199389d238a04dec9eb5398a83826686bebb5
Erik Tontini <erik_pelissari123@hotmail.com>
2026-04-06 19:49:05 -0300

    teste conecçao banco de dados

--- src/main/java/model/Venda.java
+++ src/main/java/model/Venda.java
@@ -30,34 +30,12 @@ public class Venda {
 
     }
 
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public double getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(double valorTotal) {
-        this.valorTotal = valorTotal;
-    }
+    public Produto getProduto() {return produto;}
+    public void setProduto(Produto produto) {this.produto = produto;}
+    public Usuario getUsuario() {return usuario;}
+    public void setUsuario(Usuario usuario) {this.usuario = usuario;}
+    public int getQuantidade() {return quantidade;}
+    public void setQuantidade(int quantidade) {this.quantidade = quantidade;}
+    public double getValorTotal() {return valorTotal;}
+    public void setValorTotal(double valorTotal) {this.valorTotal = valorTotal;}
 }
