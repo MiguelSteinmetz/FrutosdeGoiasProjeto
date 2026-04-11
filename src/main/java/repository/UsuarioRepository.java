02d690ce62b1afce89b7172658ed901bee43a6d1
MiguelSteinmetz <127700755+MiguelSteinmetz@users.noreply.github.com>
2026-04-10 21:49:17 -0300

    Add files via upload

--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,23 +1,25 @@
-package repository;
+    package repository;
 
-import jakarta.persistence.EntityManager;
-import model.Usuario;
+    import jakarta.persistence.EntityManager;
+    import model.Produto;
+    import model.Usuario;
 
-import java.util.List;
+    import java.util.List;
 
-public class UsuarioRepository {
+    public class UsuarioRepository {
 
-    private EntityManager em = CustomizerFactory.getEntityManager();
+        private EntityManager em = CustomizerFactory.getEntityManager();
 
-    public void salvar(Usuario usuario) {
-        this.em.getTransaction().begin();
-        this.em.persist(usuario);
-        this.em.getTransaction().commit();
+        public void salvar(Usuario usuario) {
+            this.em.getTransaction().begin();
+            this.em.persist(usuario);
+            this.em.getTransaction().commit();
+        }
+
+        public List<Usuario> buscartodos() {
+            return this.em.createQuery("select u From Usuario u", Usuario.class).getResultList();
+        }
+
+
+
     }
-
-    public List<Usuario> buscartodos() {
-        return this.em.createQuery("select u From Usuario u", Usuario.class).getResultList();
-    }
-
-
-}
